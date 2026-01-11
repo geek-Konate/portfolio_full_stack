2a625b5de661469d48e2ca86c605120fdff54cbd
Mamadou <kmma.960@gmail.com>
2026-01-11 16:41:16 +0100

    Update DATABASE_URL for Render PostgreSQL

--- src/config.js
+++ src/config.js
@@ -4,5 +4,6 @@ const isProduction = import.meta.env.PROD;
 
 // URL différente selon l'environnement
 export const BACKEND_URL = isProduction
-  ? import.meta.env.VITE_API_URL || "https://votre-backend.onrender.com"
+  ? import.meta.env.VITE_API_URL ||
+    "https://portfolio-backend-iymh.onrender.com"
   : "http://localhost:8000"; // Votre IP locale pour le dev mobile
