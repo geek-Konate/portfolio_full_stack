fef2e0071bef8b2709480ffc1ea9d7421a82eb0e
Mamadou <kmma.960@gmail.com>
2026-01-13 01:02:42 +0100

    Update backend URL for production

--- src/config.js
+++ src/config.js
@@ -1,9 +1,1 @@
-// config.js
-const isDevelopment = import.meta.env.DEV;
-const isProduction = import.meta.env.PROD;
-
-// URL différente selon l'environnement
-export const BACKEND_URL = isProduction
-  ? import.meta.env.VITE_API_URL ||
-    "https://portfolio-backend-iymh.onrender.com"
-  : "http://localhost:8000"; // Votre IP locale pour le dev mobile
+export const BACKEND_URL = "https://portfolio-backend-iymh.onrender.com";
