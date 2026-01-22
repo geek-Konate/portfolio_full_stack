7cb9bfd6da93ff8e9a34d8bb357ea58c7735c48c
Kira <kmma.960@gmail.com>
2026-01-22 23:22:45 +0100

    Changement de url de la base de donne de render a railway

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // config.js - SOLUTION SIMPLE
-export const BACKEND_URL = "https://portfolio-backend-iymh.onrender.com";
+export const BACKEND_URL = "portfolio-backend-production-5d8b.up.railway.app";
