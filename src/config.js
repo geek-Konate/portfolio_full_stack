1284d2f6b12deb2749e39456e6513525eb5ac509
Kira <kmma.960@gmail.com>
2026-01-23 13:37:07 +0100

    url de backen_url changer en railway

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // config.js - SOLUTION SIMPLE
-export const BACKEND_URL = "portfolio-backend-production-5d8b.up.railway.app";
+export const BACKEND_URL = "https://portfolio-backend-production-5d8b.up.railway.app";
