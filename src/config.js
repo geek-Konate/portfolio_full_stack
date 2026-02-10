9ba71030e4ea1bef63014c63c5b5b48746bfe87c
Kira <kmma.960@gmail.com>
2026-02-10 02:11:38 +0100

    changement de url de railay en render

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // config.js - SOLUTION SIMPLE
-export const BACKEND_URL = "https://portfolio-backend-production-5d8b.up.railway.app";
+export const BACKEND_URL = "https://portfolio-backend-iymh.onrender.com";
